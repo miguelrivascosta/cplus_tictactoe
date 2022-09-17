5c4077942d3417d441d230cc0ee83926db95e110
miguelrivascosta <rivascostam@gmail.com>
2022-09-18 00:35:14 +0200

    Add the input option checking system

--- main.cpp
+++ main.cpp
@@ -1,25 +1,57 @@
 #include <iostream>
-
+#include <string.h>
 using namespace std;
 
 int intro();
+bool checkIntroOption(string);
+bool checkStringValidValues(string, string *,int);
 int main() {
-    intro();
+    int introOption = intro();
+    cout<<"INTRO: "<<introOption<<endl;
     
     return 0;
 }
 
 int intro(){
     string introOption;
+    
     while(true){
         cout<<"1. Player vs Player"<<endl;
         cout<<"2. Player vs IA"<<endl;
         cout<<"0. Exit"<<endl;
         cout<<"CHOOSE AN OPTION: ";
         getline(cin, introOption, '\n');
-        cout<<"You have chosen: "<<introOption<<endl;
+
+        if(checkIntroOption(introOption)){
+            cout<<"You have chosen: "<<introOption<<endl;
+            return stoi(introOption);
+        }else{
+            continue;
+        }
         
     }
+}
 
-    return 0;
+bool checkIntroOption(string introOption){
+    string validInputValues[3] = {"0","1","2"};
+    if(introOption.length() > 1){
+        cout<<"You have chosen an invalid value, it has more than 1 character!"<<endl;
+        return false;
+    }
+    if(!checkStringValidValues(introOption,validInputValues,3)){
+        cout<<"You have chosen an invalid character, it should be 0, 1 or 2!"<<endl;
+        return false;
+    }
+    return true;
+}
+
+bool checkStringValidValues(string value,string * validValues, int size){
+
+    for (int i = 0; i < size; i++)
+    {
+        if(value.compare(validValues[i]) == 0){
+            return true;
+        }
+    }
+    return false;
 }
